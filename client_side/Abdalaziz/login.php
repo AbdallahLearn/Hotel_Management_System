42fbb2f85c86d6c1091f5eb6b22e981a17c6733d
comsaziz <comsaziz2@gmail.com>
2023-12-15 21:59:00 +0300

    init

--- client_side/Abdalaziz/login.php
+++ client_side/Abdalaziz/login.php
@@ -18,22 +18,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-    // Prepare and execute a query to fetch the customer's details
-    $sql = "SELECT * FROM customers 
-            WHERE email = '$email' AND password = '$password';";
+    // Fetch the hashed password from the database
+    $sql = "SELECT * FROM customers WHERE email = '$email'";
     $result = mysqli_query($conn, $sql);
 
-    if (mysqli_num_rows($result) == 1) {
-        // Customer's credentials are correct, proceed to the dashboard or authorized area
-        // Redirect to the dashboard or authorized area
-       
-        header("Location: ../Mohammad/main.html");
-        exit();
-    } else {
-        // Invalid credentials
-        echo "<p style='color:red'>" . "Invalid email or password!" . "</p>";
-    }
+    if ($result && mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        $hashed_password = $row['password'];
+
+        // Verify the password
+        if (password_verify($password, $hashed_password)) {
+            // Password is correct, proceed to the dashboard or authorized area
+            header("Location: ../Mohammad/main.html");
+            exit();
+        } else {
+            // Invalid credentials
+            echo "<p style='color:red'>" . "Invalid email or password!" . "</p>";
+        }
+    
+}
+
 }
 
 // Close the database connection
 mysqli_close($conn);
+?>
