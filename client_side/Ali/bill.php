cd30f7677a124d9cc0102d85e3c58a4e6dc907aa
ali <alitalal096@gmail.com>
2023-12-18 15:14:21 +0300

    some change

--- client_side/Ali/bill.php
+++ client_side/Ali/bill.php
@@ -6,7 +6,7 @@ include './conn.php';
 
 $booking_id = isset($_GET['booking_id']) ? $_GET['booking_id'] : null;
 
-$sql_s = 'SELECT `booking`.`arrival_date`, `booking`.`departure_date`, `room`.`category`, `room`.`price`, `hotel`.`hotel_name`, `city`.`city_name`
+$sql_s = 'SELECT `booking`.`arrival_date`, `booking`.`departure_date`, `booking`.`num_children`, `booking`.`num_adults`,`room`.`category`, `room`.`price`, `hotel`.`hotel_name`, `city`.`city_name`
 FROM `booking` 
 	LEFT JOIN `room` ON `room`.`booking_id` = `booking`.`booking_id` 
 	LEFT JOIN `hotel` ON `room`.`hotel_id` = `hotel`.`hotel_id` 
@@ -16,7 +16,7 @@ FROM `booking`
 
 $result = mysqli_query($conn, $sql_s);
 $billinfo = mysqli_fetch_all($result, MYSQLI_ASSOC);
-print_r($billinfo);
+
 mysqli_free_result($result);
 mysqli_close($conn);
 
@@ -25,6 +25,8 @@ $h_naem = htmlspecialchars($ifo['hotel_name']);
 $room_catrgory = htmlspecialchars($ifo['category']);
 $check_in = htmlspecialchars($ifo['arrival_date']);
 $check_out = htmlspecialchars($ifo['departure_date']);
+$num_children = htmlspecialchars($ifo['num_children']);
+$num_adults = htmlspecialchars($ifo['num_adults']);
 $price = htmlspecialchars($ifo['price']);
 $tax = $price * 0.15;
 $total = $price + $tax;
@@ -52,6 +54,11 @@ $booking_date = "10/12/2023";
             <a href="../../signup">Sign Up</a>
         </div>
     </div>
+
+    <div class="nav">
+        <a href="../../../city.html">Home</a> > <a href="../../../hotel_makkah">Hotel</a> > <a href="../../../booking">Booking</a> > <a href="#">Bill</a>
+    </div>
+
     <div class="container">
         <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light shadow-lg" id="box">
             <div class="col-md-50 p-lg-2 mx-auto my-1">
@@ -64,8 +71,9 @@ $booking_date = "10/12/2023";
                     <h3 class="text-center">Check in: </h3><h5><?php echo $check_in ?></h5>
                     <h3 class="text-center">Check out: </h3><h5><?php echo $check_out ?></h5>
                 </div>
-                <div class="bookingdate">
-                    <h3 class="text-center">Booking date: </h3><h5><?php echo $booking_date ?></h5>
+                <div id="check">
+                    <h3 class="text-center">number adults: </h3><h5><?php echo $num_adults ?></h5>
+                    <h3 class="text-center">number children: </h3><h5><?php echo $num_children ?></h5>
                 </div>
                 <div class="price">
                     <div class="group">
@@ -86,12 +94,14 @@ $booking_date = "10/12/2023";
 
                 <br>
                 <div class="form-group">
-                    <button id="check_availability" type="submit"><a href="./payment.php?booking_id=<?php echo $booking_id; ?>">Complete payment</a></button>
+                    <button id="check_availability" type="submit"><a href="./payment.php">Complete payment</a></button>
                 </div>
             <div class="product-device shadow-sm d-none d-md-block"></div>
             <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
         </div>
     </div>
-    
+    <footer>
+        <p>The website created by <strong>Abdallah</strong>  ,  <strong>Abdalaziz</strong>   ,  <strong>Ali</strong>   ,   <strong>Mohammad</strong></p>
+    </footer>
 </body>
 </html>
